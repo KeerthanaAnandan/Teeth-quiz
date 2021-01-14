89ea63dfe1059f14b09a601518673fc904728cb5
keerthanaAnandan <keerthanaanandanak@gmail.com>
2021-01-14 22:07:14 +0530

    initia commit

---
+++ dist/js/site.js
@@ -0,0 +1,205 @@
+let questions = [
+  {
+    id: 1,
+    question: "How many times do you brush your teeth in a day?",
+    answer: "2",
+    options: [" None", "1 time", "2 times", "3+ times"],
+  },
+  {
+    id: 2,
+    question: "Do you have regular checks up with your dentist?",
+    answer: "2",
+    options: ["Yes", " No"],
+  },
+  {
+    id: 3,
+    question: "Do you have braces or Invisalign?",
+    answer: "2",
+    options: ["None", "Braces", "Invisalign", "Both"],
+  },
+  {
+    id: 4,
+    question: "How many times do you floss in a day?",
+    answer: "2",
+    options: [" None", "One", "Two", "Three +"],
+  },
+  {
+    id: 5,
+    question: "How often do you smoke in a day?",
+    answer: "2",
+    options: ["None", "1-2", "3-4", "5+"],
+  },
+  {
+    id: 6,
+    question: "How many units of alcohol do you drink a week?",
+    answer: "2",
+    options: ["None", "1-3 units", "4-6 units", "7+ units"],
+  },
+  {
+    id: 7,
+    question: "How often do you drink fizzy drinks?",
+    answer: "2",
+    options: ["Not at all", "On special occasions", "Everyday"],
+  },
+  {
+    id: 8,
+    question: "How many liters of water do you drink in a day?",
+    answer: "2",
+    options: ["Less than 1 litre", "1-2 litres", "3-4 litres", "5+ litres"],
+  },
+  {
+    id: 9,
+    question: "Have you looked into teeth whitening before?",
+    answer: "2",
+    options: ["Yes", "No"],
+  },
+  {
+    id: 10,
+    question: "Have you tried other teeth whitening product?",
+    answer: "2",
+    options: ["Yes", "No"],
+  },
+];
+
+let question_count = 0;
+//let points = 0;
+Max_Questions = 10;
+let firstTypePoints = 0;
+let secondTypePoints = 0;
+let thirdTypePoints = 0;
+
+window.onload = function () {
+  show(question_count);
+};
+
+function next(e) {
+  // if the question is last then redirect to final page
+  if (question_count == questions.length - 1) {
+    sessionStorage.setItem("time", time);
+    clearInterval(mytime);
+    location.href = "form.html";
+  }
+  console.log(question_count);
+
+  let user_answer = document.querySelector(".option.active").innerHTML;
+
+  //check answer
+  /////////////////////////first type
+  if (
+    user_answer === "2 times" ||
+    user_answer === "3+ times" ||
+    user_answer === "Yes" ||
+    user_answer === "None" ||
+    user_answer === "Two" ||
+    user_answer === "Three +" ||
+    user_answer === "1-3 units" ||
+    user_answer === "Not at all" ||
+    user_answer === "On special occasions" ||
+    user_answer === "3-4 litres" ||
+    user_answer === "5+ litres" ||
+    user_answer === "No"
+  ) {
+    firstTypePoints += 1;
+  }
+  /////////////////second type
+  else if (
+    user_answer === "1 time" ||
+    user_answer === " No" ||
+    user_answer === "Braces" ||
+    user_answer === "Invisalign" ||
+    user_answer === "Both" ||
+    user_answer === "One" ||
+    user_answer === "1-2" ||
+    user_answer === "4-6 units" ||
+    user_answer === "1-2 litres"
+  ) {
+    secondTypePoints += 1;
+  }
+  ///////////////////third type
+  else if (
+    user_answer === " None" ||
+    user_answer === "3-4" ||
+    user_answer === "5+" ||
+    user_answer === "7+ units" ||
+    user_answer === "Everyday" ||
+    user_answer === "Less than 1 litre"
+  ) {
+    thirdTypePoints += 1;
+  } //////////////////the influencer
+
+  sessionStorage.setItem("firstTypePoints", firstTypePoints);
+  sessionStorage.setItem("secondTypePoints", secondTypePoints);
+  sessionStorage.setItem("thirdTypePoints", thirdTypePoints);
+
+  //let coloranswer = document.querySelector("li.option.active");
+  //e.target.coloranswer.classList.add("correct");
+
+  question_count++;
+  show(question_count);
+  toggleActive();
+  //Update the progress Bar
+
+  const progressBarFull = document.getElementById("progressBarFull");
+
+  progressBarFull.style.width = ` ${(question_count / Max_Questions) * 100}%`;
+}
+
+function show(count) {
+  //progress text
+  const progressText = document.getElementById("progressText");
+
+  progressText.innerText = `Question ${count + 1}/${Max_Questions}`;
+
+  let question = document.getElementById("questions");
+  let [first, second, third, fourth] = questions[count].options;
+  ///////no of questions
+  if (question_count == 1 || question_count == 8 || question_count == 9) {
+    question.innerHTML = `
+    <h2> ${questions[count].question}</h2>
+     <ul class="option_group">
+    <li><span class="option">${first}</span></li>
+    <li ><span class="option">${second}</span></li>
+    
+
+  </ul> 
+    `;
+    toggleActive();
+  } else if (question_count == 6) {
+    question.innerHTML = `
+    <h2> ${questions[count].question}</h2>
+     <ul class="option_group">
+    <li><span class="option">${first}</span></li>
+    <li ><span class="option">${second}</span></li>
+    <li ><span class="option">${third}</span></li>
+    
+
+  </ul> 
+    `;
+    toggleActive();
+  } else {
+    question.innerHTML = `
+  <h2> ${questions[count].question}</h2>
+   <ul class="option_group">
+  <li><span class="option">${first}</span></li>
+  <li ><span class="option">${second}</span></li>
+  <li ><span class="option">${third}</span></li>
+  <li ><span class="option">${fourth}</span></li>
+</ul> 
+  `;
+    toggleActive();
+  }
+}
+function toggleActive() {
+  let option = document.querySelectorAll(".option");
+
+  for (let i = 0; i < option.length; i++) {
+    option[i].onclick = function () {
+      for (let i = 0; i < option.length; i++) {
+        if (option[i].classList.contains("active")) {
+          option[i].classList.remove("active");
+        }
+      }
+      option[i].classList.add("active");
+    };
+  }
+}
---
+++ dist/js/userInfo.js
@@ -0,0 +1,156 @@
+let firstTypePoints = sessionStorage.getItem("firstTypePoints");
+let secondTypePoints = sessionStorage.getItem("secondTypePoints");
+let thirdTypePoints = sessionStorage.getItem("thirdTypePoints");
+
+let pointop = 0;
+let mess;
+let typo;
+//first
+if (firstTypePoints > secondTypePoints && firstTypePoints > thirdTypePoints) {
+  typo = "Great(type 1)";
+  mess = [
+    "After looking at your answers, we can tell that you look after your teeth very well.",
+    " Polardent would be a great to help brighten your teeth....",
+  ];
+}
+
+//second
+if (secondTypePoints > firstTypePoints && secondTypePoints > thirdTypePoints) {
+  typo = "Type 2";
+  mess = [
+    "It seems you may need to improve your oral hygiene.",
+    " Never fear Polardent is here and we can help brighten up your teeth.",
+    " Here are some tips on how to help you gain healthier teeth...",
+  ];
+}
+//third
+else if (
+  thirdTypePoints > firstTypePoints &&
+  thirdTypePoints > secondTypePoints
+) {
+  typo = "Type 3";
+  mess = [
+    "It seems looking at your results that due to wear braces or Invisalign it is best to wait until your treatment is complete before proceeding with Polardent teeth whitening kits to gain the best possible results.",
+    "However here are some helpful tips on how to keep up your oral hygiene....",
+  ];
+}
+
+const mao = mess.map((ms) => `<li>` + ms + `</li>`).join("");
+
+document.querySelector(".headmsg").innerHTML = typo;
+const msgme = document.querySelector(".msghere");
+msgme.className = "center-item";
+//document.querySelector(".msghere").innerHTML = mao;
+
+msgme.innerHTML = mao;
+document.querySelector(".usertype").value = typo;
+
+document.querySelector(".usertype").style.display = "none";
+
+//formspree
+
+window.addEventListener("DOMContentLoaded", function () {
+  // get the form elements defined in your form HTML above
+
+  var form = document.getElementById("my-form");
+  //var button = document.getElementById("my-form-button");
+  var status = document.getElementById("status");
+
+  // Success and Error functions for after the form is submitted
+
+  function success() {
+    form.reset();
+    // button.style = "display: block ";
+    status.classList.add("success");
+    status.innerHTML = "Submitted Successfully!";
+  }
+
+  function error() {
+    status.classList.add("error");
+    status.innerHTML = "Oops! There was a problem.";
+  }
+
+  // handle the form submission event
+
+  form.addEventListener("submit", function (ev) {
+    ev.preventDefault();
+    var data = new FormData(form);
+    ajax(form.method, form.action, data, success, error);
+  });
+});
+
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
+}
+//share buttons
+/* 
+
+Social Share Links:
+
+WhatsApp:
+https://wa.me/?text=[post-title] [post-url]
+
+Facebook:
+https://www.facebook.com/sharer.php?u=[post-url]
+
+Twitter:
+https://twitter.com/share?url=[post-url]&text=[post-title]
+
+Pinterest:
+https://pinterest.com/pin/create/bookmarklet/?media=[post-img]&url=[post-url]&is_video=[is_video]&description=[post-title]
+
+LinkedIn:
+https://www.linkedin.com/shareArticle?url=[post-url]&title=[post-title]
+
+*/
+
+const facebookBtn = document.querySelector(".facebook-btn");
+const twitterBtn = document.querySelector(".twitter-btn");
+
+const linkedinBtn = document.querySelector(".linkedin-btn");
+const whatsappBtn = document.querySelector(".whatsapp-btn");
+
+function init() {
+  let postUrl = encodeURI(document.location.href);
+  let postTitle = encodeURI("Checkout what type of intagrammer I am ");
+  // let postImg = encodeURI(pinterestImg.src);
+
+  facebookBtn.setAttribute(
+    "href",
+    `https://www.facebook.com/sharer.php?u=${postUrl}`
+  );
+
+  twitterBtn.setAttribute(
+    "href",
+
+    `https://twitter.com/share?url=${postUrl}&text=${postTitle}`
+  );
+
+  // linkedinBtn.setAttribute(
+  // "href",
+  //"http://www.linkedin.com/shareArticle?mini=true&amp;url=https://simplesharebuttons.com"
+  //`https://www.linkedin.com/shareArticle?url=${postUrl}&title=${postTitle}`
+  //);
+  linkedinBtn.setAttribute(
+    "href",
+    `https://www.linkedin.com/shareArticle?url=${postUrl}&title=${postTitle}`
+  );
+
+  whatsappBtn.setAttribute(
+    "href",
+    `https://wa.me/?text=${postTitle} ${postUrl}`
+  );
+}
+
+init();
